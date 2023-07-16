26d77ef468f8b7206947776ae5d4f26d6e64f908
gracebir <grace.devolop@gmail.com>
2023-07-16 21:58:10 +0200

    define colors values and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'shoulder': ['Big Shoulders Display', 'cursive'],
+        'lexend': ['Lexend Deca', 'sans-serif']
+      },
+      colors:{
+        'bright-orange':'hsl(31, 77%, 52%)',
+        'dark-cyan':'hsl(184, 100%, 22%)',
+        'very-dark-cyan':'hsl(179, 100%, 13%)',
+      }
+    },
   },
   plugins: [],
 }
